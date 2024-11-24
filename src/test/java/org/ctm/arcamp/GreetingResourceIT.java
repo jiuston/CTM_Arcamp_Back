62c8f748744fa1a4e76234774d68289128bb8e38
quarkusio <no-reply@quarkus.io>
2024-11-24 11:23:36 +0000

    Initial commit

---
+++ src/test/java/org/ctm/arcamp/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.ctm.arcamp;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
